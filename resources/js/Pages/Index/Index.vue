1299d3cbad71eb81e58e1e0691d5524fd8823dee
coenez <ccpstals@hotmail.com>
2024-03-16 12:28:52 +0100

    Add vue basic components

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,9 +1,12 @@
 <template>
     <div>Index</div>
-    <Link href="/hello">Show page</Link>
-
+        <Link href="/hello">Show page</Link>
+    <div>message: {{message}}</div>
 </template>
 
 <script setup>
 import {Link} from '@inertiajs/vue3'
+defineProps({
+    message: String
+});
 </script>
--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -1,9 +1,9 @@
 <template>
     <div>Show</div>
     <Link href="/">index page</Link>
-
 </template>
 
 <script setup>
 import {Link} from '@inertiajs/vue3'
+
 </script>
