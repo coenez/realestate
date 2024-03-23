26644e86d0c069f954ec9a4ac1f7f8eba35a6dc7
coenez <ccpstals@hotmail.com>
2024-03-23 13:49:40 +0100

    Refactor existing routes to ziggy route. Add edit and delete buttons

--- resources/js/Pages/Listing/Edit.vue
+++ resources/js/Pages/Listing/Edit.vue
@@ -1,5 +1,5 @@
 <template>
-    <form @submit.prevent="create">
+    <form @submit.prevent="update">
         <div>
             <div>
                 <label>Beds</label>
@@ -50,7 +50,7 @@
             </div>
 
             <div>
-                <button type="submit">Create</button>
+                <button type="submit">Update</button>
             </div>
         </div>
     </form>
@@ -59,18 +59,13 @@
 <script setup>
 import {useForm} from '@inertiajs/vue3'
 
-    const form = useForm({
-        beds: 0,
-        baths: 0,
-        area: 0,
-        city: null,
-        code: null,
-        street: null,
-        street_nr: null,
-        price: 0,
-    });
+const props = defineProps({
+    listing: Object
+})
 
-    const create = () => form.post(route('listing.store'));
+const form = useForm(props.listing);
+
+const update = () => form.put(route('listing.update', {listing: props.listing.id}));
 </script>
 
 <style scoped>
