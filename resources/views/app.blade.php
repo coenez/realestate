1923642d96e86f462ddcee5c1d1b5379fe214af3
coenez <ccpstals@hotmail.com>
2024-03-27 14:24:56 +0100

    add progressbar

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Real estate course</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" />
 
         @vite(['resources/js/app.js'])
         @inertiaHead
