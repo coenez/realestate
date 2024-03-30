7643a4c21248bcdd917dabeaea2bb98671cd5dbd
coenez <ccpstals@hotmail.com>
2024-03-30 11:08:49 +0100

    Add monthly payment to index by extracting the index listing to separate component

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,30 +1,14 @@
-<template>
-    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-        <Box v-for="listing in listings" :key="listing.id">
-            <Link :href="route('listing.show', {listing: listing.id})">
-                <Price :price="listing.price" class="text-2xl font-bold"/>
-                <ListingSpace :listing="listing" class="text-lg" />
-                <ListingAddress :listing="listing" class="text-gray-400"/>
-            </Link>
-            <div>
-                <Link :href="route('listing.edit', {listing: listing.id})" as="button">Edit</Link>
-            </div>
-            <div>
-                <Link :href="route('listing.destroy', {listing: listing.id})" method="delete" as="button">Delete</Link>
-            </div>
-        </Box>
-    </div>
-
-</template>
-
 <script setup>
-import {Link} from '@inertiajs/vue3'
-import ListingAddress from "../../Component/ListingAddress.vue";
-import Box from "../../Component/UI/Box.vue";
-import ListingSpace from "../../Component/ListingSpace.vue";
-import Price from "../../Component/Price.vue";
+import Listing from "./Index/Components/Listing.vue";
 
 defineProps({
     listings: Array
 });
 </script>
+
+<template>
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+        <Listing v-for="listing in listings" :key="listing.id" :listing="listing" />
+    </div>
+
+</template>
---
+++ resources/js/Pages/Listing/Index/Components/Listing.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+import ListingSpace from "../../../../Component/ListingSpace.vue";
+import Price from "../../../../Component/Price.vue";
+import Box from "../../../../Component/UI/Box.vue";
+import ListingAddress from "../../../../Component/ListingAddress.vue";
+import {Link} from "@inertiajs/vue3";
+import {useMonthlyPayment} from "../../../../Composables/useMonthlyPayment.js";
+
+const props = defineProps({
+    listing: Object
+});
+
+const monthlyPayment = useMonthlyPayment(props.listing.price, 2.5, 25);
+</script>
+
+<template>
+    <Box>
+        <Link :href="route('listing.show', {listing: listing.id})">
+            <div class="flex items-center gap-1">
+                <Price :price="listing.price" class="text-2xl font-bold"/>
+                <div class="text-xs text-gray-500">
+                    <Price :price="monthlyPayment" /> pm
+                </div>
+            </div>
+
+            <ListingSpace :listing="listing" class="text-lg" />
+            <ListingAddress :listing="listing" class="text-gray-400"/>
+        </Link>
+        <div>
+            <Link :href="route('listing.edit', {listing: listing.id})" as="button">Edit</Link>
+        </div>
+        <div>
+            <Link :href="route('listing.destroy', {listing: listing.id})" method="delete" as="button">Delete</Link>
+        </div>
+    </Box>
+</template>
