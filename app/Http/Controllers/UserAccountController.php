78424674d0d385a49fa21c8208a82243cdba86fc
coenez <ccpstals@hotmail.com>
2024-03-30 14:39:58 +0100

    Add backend and frontend functionality to create a new user account and on creation, login that account.

---
+++ app/Http/Controllers/UserAccountController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserAccountController extends Controller
+{
+    public function create()
+    {
+        return inertia('UserAccount/Create');
+    }
+
+    public function store(Request $request) {
+        $toValidate = [
+            'name' => 'required|string',
+            'email' => 'required|unique:users|email',
+            'password' => 'required|min:8|confirmed',
+        ];
+
+        $user = User::make($request->validate($toValidate));
+        $user->password = Hash::make($user->password);
+        $user->save();
+
+        Auth::login($user);
+
+        return redirect()->route('listing.index')
+            ->with('success', 'Account created!');
+    }
+}
---
+++ resources/js/Pages/UserAccount/Create.vue
@@ -0,0 +1,41 @@
+<script setup>
+import {useForm} from '@inertiajs/vue3'
+
+const form = useForm({
+    name: null,
+    email: null,
+    password: null,
+    password_confirmation: null
+});
+
+const register = () => form.post(route('user-account.store'));
+</script>
+
+<template>
+    <form @submit.prevent="register">
+        <div class="w-1/2 mx-auto">
+            <div>
+                <label for="email" class="label">Name</label>
+                <input id="email" class="inputField" type="text" v-model="form.name"/>
+                <div class="inputError" v-if="form.errors.name">{{form.errors.name}}</div>
+            </div>
+            <div class="mt-4">
+                <label for="email" class="label">E-mail</label>
+                <input id="email" class="inputField" type="text" v-model="form.email"/>
+                <div class="inputError" v-if="form.errors.email">{{form.errors.email}}</div>
+            </div>
+            <div class="mt-4">
+                <label for="password" class="label">Password</label>
+                <input id="password" class="inputField" type="password" v-model="form.password"/>
+                <div class="inputError" v-if="form.errors.password">{{form.errors.password}}</div>
+            </div>
+            <div class="mt-4">
+                <label for="password_confirmation" class="label">Password confirmation</label>
+                <input id="password_confirmation" class="inputField" type="password" v-model="form.password_confirmation"/>
+            </div>
+            <div class="mt-4">
+                <button class="button w-full" type="submit">Create account</button>
+            </div>
+        </div>
+    </form>
+</template>
