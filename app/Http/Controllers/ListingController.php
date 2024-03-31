cb1833c8bf2e53d7cc22dfc3720b9a9b1897580a
coenez <ccpstals@hotmail.com>
2024-03-31 17:25:41 +0200

    Add and implement basic policy for users and listings

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -9,6 +9,7 @@ class ListingController extends Controller
 {
     public function __construct() {
         $this->middleware('auth')->except(['index', 'show']);
+        $this->authorizeResource(Listing::class, 'listing');
     }
 
     /**
