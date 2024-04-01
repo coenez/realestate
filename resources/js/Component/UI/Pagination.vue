ae93a97bf95233d739d234cba0ceb02149ffb519
coenez <ccpstals@hotmail.com>
2024-04-01 12:18:13 +0200

    Add pagination and filtering components

---
+++ resources/js/Component/UI/Pagination.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {Link} from "@inertiajs/vue3";
+
+defineProps({links: Array});
+</script>
+
+<template>
+    <div class="flex gap-1">
+        <Link v-for="(link, index) in links" :key="index" :href="link.url"
+              class="py-2 px-4 rounded-md" :class="{'bg-indigo-500 dark:bg-indigo-800 text-gray-300': link.active}" v-html="link.label"
+        />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
