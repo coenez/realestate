9dfa498f481c77c635b75f99ca07c2db03c6846e
coenez <ccpstals@hotmail.com>
2024-04-07 13:57:50 +0200

    Some ux and styling

--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -1,12 +1,15 @@
 <script setup>
     import Box from "../../../Component/UI/Box.vue";
     import {useForm} from '@inertiajs/vue3'
+    import {computed} from "vue";
 
     const props = defineProps({listing: Object});
 
     const form = useForm({
         images: []
     });
+
+    const uploadAvailable = computed(() => form.images.length);
 
     const reset = () => form.reset('images');
 
@@ -31,9 +34,11 @@
     <Box>
         <template #header>Upload images</template>
         <form @submit.prevent="upload">
-            <input type="file" multiple @input="addFiles" />
-            <button type="submit" class="btn-outline">Upload</button>
-            <button type="reset" class="btn-outline" @click="reset">Reset</button>
+            <section class="flex items-center gap-2 my-4">
+                <input type="file" multiple @input="addFiles" name="images" class="border rounded-md border-gray-200 file:hover:cursor-pointer file:hover:bg-gray-200 file:px-4 file:py-2 file:text-gray-500 file:border-0 file:font-medium"/>
+                <button :disabled="!uploadAvailable" type="submit" class="btn-outline disabled:opacity-25 disabled:cursor-not-allowed">Upload</button>
+                <button type="reset" class="btn-outline" @click="reset">Reset</button>
+            </section>
         </form>
     </Box>
 </template>
