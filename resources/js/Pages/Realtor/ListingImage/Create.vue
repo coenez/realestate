afcfda0e4260e08ff3c602335e745dbb00df831c
coenez <ccpstals@hotmail.com>
2024-04-07 16:18:07 +0200

    Show images on the listing detail page

--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -3,6 +3,7 @@
     import {useForm} from '@inertiajs/vue3'
     import {computed} from "vue";
     import {Link} from "@inertiajs/vue3";
+    import ListingAddress from "../../../Component/ListingAddress.vue";
 
     const props = defineProps({listing: Object});
 
@@ -33,7 +34,7 @@
 
 <template>
     <Box>
-        <template #header>Upload images</template>
+        <template #header>Upload images for: <ListingAddress :listing="listing" class="italic" /></template>
         <form @submit.prevent="upload">
             <section class="flex items-center gap-2 my-4">
                 <input type="file" multiple @input="addFiles" name="images" class="border rounded-md border-gray-200 file:hover:cursor-pointer file:hover:bg-gray-200 file:px-4 file:py-2 file:text-gray-500 file:border-0 file:font-medium"/>
@@ -43,7 +44,7 @@
         </form>
     </Box>
     <Box v-if="listing.images.length" class="mt-4">
-        <template #header>Current listing images</template>
+        <template #header>Current images</template>
         <section class="mt-4 grid grid-cols-3 gap-4">
             <div class="flex flex-col justify-between" v-for="image in listing.images" :key="image.id">
                 <img :alt="image.filename" :src="image.src" class="rounded-md"/>
