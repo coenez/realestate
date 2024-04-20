55260007f41b85efdf461a7aa31bcb63ea6a4dc2
coenez <ccpstals@hotmail.com>
2024-04-20 14:41:26 +0200

    Cleanup the template. Dont show accept button if listing is accepted and add an accepted label to the offer

--- resources/js/Pages/Realtor/Show.vue
+++ resources/js/Pages/Realtor/Show.vue
@@ -24,15 +24,18 @@
             <div class="w-full text-center font-medium text-gray-500">No offers</div>
         </Box>
 
-        <div v-else class="md:col-span-7 items-center">
-            <Offer v-for="offer in listing.offers" :offer="offer" :price="listing.price"  class="mt-4"/>
+        <div v-else class="md:col-span-7 flex flex-col gap-4">
+            <Offer v-for="offer in listing.offers" :offer="offer" :price="listing.price"/>
         </div>
 
-        <Box class="md:col-span-5">
-            <template #header>Basic Info</template>
-            <Price :price="listing.price" class="text-2xl font-bold" />
-            <ListingSpace :listing="listing" class="text-lg" />
-            <ListingAddress :listing="listing" class="text-gray-500" />
-        </Box>
+        <div class="md:col-span-5">
+            <Box>
+                <template #header>Basic Info</template>
+                <Price :price="listing.price" class="text-2xl font-bold" />
+                <ListingSpace :listing="listing" class="text-lg" />
+                <ListingAddress :listing="listing" class="text-gray-500" />
+            </Box>
+        </div>
+
     </section>
 </template>
--- resources/js/Pages/Realtor/Show/Components/Offer.vue
+++ resources/js/Pages/Realtor/Show/Components/Offer.vue
@@ -12,11 +12,16 @@
     const difference = computed(() => props.offer.amount - props.price);
 
     const madeOn = computed(() => new Date(props.offer.created_at).toDateString());
+
+    const sold = computed(() => !props.offer.accepted_at && !props.offer.rejected_at);
 </script>
 
 <template>
     <Box>
-        <template #header>Offer #{{ offer.id }}</template>
+        <template #header>
+            Offer #{{ offer.id }}
+            <span v-if="offer.accepted_at" class="dark:bg-green-900 dark:text-green-200 bg-green-200 text-green-900 p-1 rounded-md uppercase ml-1">accepted</span>
+        </template>
         <section class="flex items-center justify-between">
             <div>
                 <Price :price="offer.amount" class="text-xl" />
@@ -25,7 +30,7 @@
                 <div class="text-gray-500 text-sm">Made on {{ madeOn }}</div>
             </div>
             <div>
-                <Link :href="route('realtor.offer.accept', {offer: offer.id})" method="put" class="btn-outline text-xs font-medium" as="button">Accept</Link>
+                <Link v-if="sold" :href="route('realtor.offer.accept', {offer: offer.id})" method="put" class="btn-outline text-xs font-medium" as="button">Accept</Link>
             </div>
         </section>
     </Box>
